97fc9449777f17a442cc8b79f0269af36567ff52
Vastagon <jacob.yancey29@yahoo.com>
2023-03-19 01:36:30 -0500

    Started creation of styling of the popout cart. Users can now click on those items to go to them as well. Created a delete button that can be clicked over the clickable card element. Fixed visual bug with scrolling

--- src/components/CartModal.tsx
+++ src/components/CartModal.tsx
@@ -1,16 +1,34 @@
 import styles from "@/styles/CartModal.module.css"
-import { useState, useContext, JSXElementConstructor, ReactElement, ReactFragment, ReactPortal, useEffect } from "react"
+import Image from "next/image"
+import { useState, useContext, useEffect } from "react"
 import { uuid } from "uuidv4"
 import { UserContext } from "./UserContext"
+import { useRouter } from "next/router"
 
 export default function CartModal(){
+    const router = useRouter()
     const {cart} = useContext(UserContext)
     const [itemListDivs, setItemListDivs] = useState()
-    console.log(cart)
+
+    function cardOrButtonClicked(e: any, routerPath: string){
+        if(e.target.name === "cartDelButton"){
+            ///Remove item from cart here
+        }else{
+            ///Go to page here
+            router.push(`/Store/${routerPath}`)
+        }
+    }
+
     useEffect(() =>{
         setItemListDivs(cart.map((prev: any) => {
             return(
-                <div key={uuid()}>{prev}</div>
+                <div onClick={(e) => {cardOrButtonClicked(e, prev)}} className={styles.item} key={uuid()}>
+                    <Image className={styles.item_image} loader={() => "https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg"} src="https://fakestoreapi.com/img/61mtL65D4cL._AC_SX679_.jpg" alt="asd" width={10} height={10} />
+                    <div className={styles.right_side}>
+                        <p className={styles.item_name}>{prev}</p>
+                        <button name="cartDelButton" className={styles.remove_button}>Remove</button>
+                    </div>
+                </div>
             )
         }))
     }, [cart])
