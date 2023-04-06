4f67da8c157584e55d849c32a6e7514c0efbaf90
Vastagon <jacob.yancey29@yahoo.com>
2023-04-05 23:36:58 -0500

    Made one prisma client in a module and am using that one client for all api routes

---
+++ src/components/prisma.ts
@@ -0,0 +1,22 @@
+import { PrismaClient } from "@prisma/client";
+
+declare global {
+    // eslint-disable-next-line no-var
+    var prisma: PrismaClient;
+}
+
+let prisma: PrismaClient
+
+if (process.env.NODE_ENV === 'production') {
+	prisma = new PrismaClient()
+}
+// `stg` or `dev`
+else {
+	if (!global.prisma) {
+		global.prisma = new PrismaClient()
+	}
+
+	prisma = global.prisma
+}
+
+export default prisma
--- src/pages/api/addItemsToCart.ts
+++ src/pages/api/addItemsToCart.ts
@@ -1,8 +1,6 @@
 import type { NextApiRequest, NextApiResponse } from 'next'
 import { PrismaClient } from '@prisma/client';
-
-const prisma = new PrismaClient()
-
+import prisma from "../../components/prisma"
 
 type Data = {
   itemInfo: any
