435b356c5b9bf748abcdad39cb51a5c1329d40c7
Vastagon <jacob.yancey29@yahoo.com>
2023-04-27 00:54:00 -0500

    Pagination now working

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -5,6 +5,7 @@ import { getSession, SessionProvider } from "next-auth/react";
 import { UserContext } from "@/components/UserContext";
 import { useState, useEffect } from "react";
 import axios from "axios";
+import { createTheme, ThemeProvider, styled } from "@mui/material/styles";
 
 // type cartComponents = {
 //   items_uid: string
@@ -12,61 +13,79 @@ import axios from "axios";
 //   image_path: string
 // }
 
-export default function App({ Component, pageProps: {session, ...pageProps} }: AppProps) {
+export default function App({ Component, pageProps: { session, ...pageProps } }: AppProps) {
   const [cart, setCart] = useState();
   const [sessionState, setSessionState] = useState<any>();
   const [showCartModal, setShowCartModal] = useState(false);
 
+  const theme = createTheme({
+    palette: {
+      primary: {
+        light: "#3b4f6b",
+        main: "#0b2447",
+        dark: "#071931",
+        contrastText: "#fff",
+      },
+      secondary: {
+        light: "#b7dfec",
+        main: "#A5D7E8",
+        dark: "#7396a2",
+        contrastText: "#000",
+      },
+    },
+  });
 
-  async function getSessionFunction(){
+  async function getSessionFunction() {
     setSessionState(await getSession());
   }
 
-  async function addToCart(item: any, itemRoute: string){
-    if(cart){
-      if(sessionState){
+  async function addToCart(item: any, itemRoute: string) {
+    if (cart) {
+      if (sessionState) {
         ///Add to user's cart DB
-        const res = await axios.post("http://localhost:3000/api/addItemsToCart", {email: sessionState!.user!.email, itemName: itemRoute});
+        const res = await axios.post("http://localhost:3000/api/addItemsToCart", { email: sessionState!.user!.email, itemName: itemRoute });
         getCartAndCreateUser();
-      }else{
+      } else {
         ///Add to state
         // cart.push(item.title)
-      }            
+      }
     }
   }
 
-  useEffect(() =>{
+  useEffect(() => {
     getSessionFunction();
 
     ///Click listener to close cart modal when clicking outside
-    window.addEventListener("click", (e) =>{
+    window.addEventListener("click", (e) => {
       // console.log((e.target as HTMLTextAreaElement).id)
-      if((e.target as HTMLTextAreaElement).id !== "cartModal" && (e.target as HTMLTextAreaElement).id !== "cartIcon" && (e.target as HTMLTextAreaElement).name !== "cartDelButton"){
+      if ((e.target as HTMLTextAreaElement).id !== "cartModal" && (e.target as HTMLTextAreaElement).id !== "cartIcon" && (e.target as HTMLTextAreaElement).name !== "cartDelButton") {
         setShowCartModal(false);
       }
     });
   }, []);
-  
-  useEffect(() =>{
+
+  useEffect(() => {
     getCartAndCreateUser();
   }, [sessionState]);
 
-  async function getCartAndCreateUser(){
-    if(sessionState){
-      const res = await axios.post("http://localhost:3000/api/getCartAndCreateUser", {email: sessionState.user.email, username: sessionState.user.name});
+  async function getCartAndCreateUser() {
+    if (sessionState) {
+      const res = await axios.post("http://localhost:3000/api/getCartAndCreateUser", { email: sessionState.user.email, username: sessionState.user.name });
 
-      if(res.data.cart_items){
+      if (res.data.cart_items) {
         setCart(res.data.cart_items);
-      }      
+      }
     }
   }
 
-  return ( 
-    <SessionProvider session={session}>
-      <UserContext.Provider value={{addToCart, cart, setCart, sessionState}}>
-        <Navbar showCartModal={showCartModal} setShowCartModal={setShowCartModal} />
-        <Component {...pageProps} />
-      </UserContext.Provider>
-    </SessionProvider>
+  return (
+    <ThemeProvider theme={theme}>
+      <SessionProvider session={session}>
+        <UserContext.Provider value={{ addToCart, cart, setCart, sessionState }}>
+          <Navbar showCartModal={showCartModal} setShowCartModal={setShowCartModal} />
+          <Component {...pageProps} />
+        </UserContext.Provider>
+      </SessionProvider>
+    </ThemeProvider>
   );
 }
--- src/pages/api/getStoreCards.ts
+++ src/pages/api/getStoreCards.ts
@@ -3,22 +3,26 @@ import prisma from "../../components/prisma";
 
 
 type Data = {
-  storeCards: any
+  pageItems: any
+  totalPages: number
 }
 
-async function returnCards() {
-  // const data = await prisma.items.findMany()
-  // const res = await prisma.$queryRaw`SELECT * FROM items ORDER BY rating DESC LIMIT 20`;
+async function returnCards(pageClicked: number) {
+  const pageData = await prisma.$queryRaw`SELECT * FROM items LIMIT 20 OFFSET ${pageClicked * 20}`;
 
-  const res = await prisma.$queryRaw`SELECT * FROM items LIMIT 20`;
+  let totalItems: any = await prisma.$queryRaw`SELECT COUNT(*) as totalitems FROM items;`;
+  ///Normal query returns a bigInt, so I need to do this to get rid of the n at the end and convert it to a number I can use
+  totalItems = parseInt(totalItems[0].totalitems.toString());
 
-  return res;
+  const totalPages = Math.floor(totalItems / 20);
+
+  return { pageData, totalPages };
 }
 
 export default async function handler(
   req: NextApiRequest,
   res: NextApiResponse<Data>
 ) {
-  const response = await returnCards();
-  res.status(200).json({ storeCards: response });
+  const response = await returnCards(req.body.pageClicked);
+  res.status(200).json({ pageItems: response.pageData, totalPages: response.totalPages });
 }
