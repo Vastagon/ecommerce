464d71be49f1f79e711b99377b56e96638beee1f
Vastagon <jacob.yancey29@yahoo.com>
2023-04-27 17:22:02 -0500

    Trying to get rid of CORS error happening only in production

--- next.config.js
+++ next.config.js
@@ -1,6 +1,20 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
+  async headers() {
+    return [
+      {
+        // matching all API routes
+        source: "/api/:path*",
+        headers: [
+          { key: "Access-Control-Allow-Credentials", value: "true" },
+          { key: "Access-Control-Allow-Origin", value: "https://ecommerce-elsg86sju-vastagon.vercel.app" },
+          { key: "Access-Control-Allow-Methods", value: "GET,OPTIONS,PATCH,DELETE,POST,PUT" },
+          { key: "Access-Control-Allow-Headers", value: "X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version" },
+        ]
+      }
+    ]
+  },
   images: {
     domains: [
       'lh3.googleusercontent.com',
@@ -8,7 +22,6 @@ const nextConfig = {
       'fakestoreapi.com',
       'dummyimage.com'
     ],
-
     remotePatterns: [
       {
         protocol: 'https',
